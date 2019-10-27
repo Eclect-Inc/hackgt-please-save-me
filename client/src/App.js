47e9b490f91ed0f15614f69e56f4988035a65456
vtang7 <vietfu@eclect.xyz>
2019-10-27 01:02:27 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Demo } from './demo.js';
+import { Button } from 'antd';
+import { Input } from 'antd';
+import { Icon } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+import { white } from 'ansi-colors';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Demo />
-        <p>Improving Accessibility</p>
+        <div className="input-panel">
+          <p>Improving Accessibility via SMS</p>
+          <div className="input">
+            <Input placeholder="Input message here." style={{marginRight: 10}}/>
+            <Button type="primary" shape="square" icon="double-right"/>
+          </div>
+        </div>
+        <div style={{backgroundColor: white, height: 50, width: 50}}>
+
+        </div>
       </header>
     </div>
   );
--- client/src/demo.js
+++ client/src/demo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Phone from './phone.png';
 import './App.css';
 
